03d36171996cc25f1ea12211a3547ba573130d8e
Nick Lemmon <nicklemmonbbt@gmail.com>
2017-11-20 21:29:11 -0500

    Adding new components, updating design

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import GoCheck from 'react-icons/lib/go/check';
 import styles from './Card.css';
 
 function CardList( props ) {
   const itemContents = props.itemContents;
   const itemClickMethod = props.itemClickMethod;
-  const listItems = itemContents.map( ( itemContent ) =>
-    <li 
-      onClick={ itemClickMethod } 
-      className={ styles.CardListItem } 
-      key={ `${itemContent}-${Math.random(1, 100)}` } 
-      value={ itemContent } 
-      tabIndex="0" 
-      role="button">
-      { itemContent }
-    </li>
-  );
+
+  let listItems;
+
+  if ( itemContents.length < 1 ) {
+    listItems = <div className={ styles.CardContent }>No items found.</div>
+  } else {
+    listItems = itemContents.map( ( itemContent ) =>
+        <li 
+          onClick={ itemClickMethod } 
+          className={ styles.CardListItem } 
+          key={ `${itemContent}-${Math.random(1, 100)}` } 
+          value={ itemContent } 
+          tabIndex="0" 
+          role="button">
+          { itemContent }
+
+          <GoCheck className={ styles.CardListIcon}/>
+        </li>
+      );
+    }
 
   return (
     <ul className={ styles.CardList }>
